a08cc1c3a2cf0ecc26c232c877d5f4e8f71dbddd
lmj_java <lmj@kipis.co.kr>
2020-08-28 13:39:17 +0900

    WebSoket을 SoketIo class로 변경 연결되고 데이터 넘어오는것 까지 확인함 (http url을 연결하기 위한 설정을 android에 해줬음, 기억에는 ios에도 해줘야 하는걸로 기억함 아직 안함)

--- lib/navigator/09_webSocket.dart
+++ lib/navigator/09_webSocket.dart
@@ -1,6 +1,7 @@
+
 import 'package:flutter/material.dart';
-import 'package:web_socket_channel/io.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:flutter_socket_io/flutter_socket_io.dart';
+import 'package:flutter_socket_io/socket_io_manager.dart';
 
 class WebSocketView extends StatefulWidget {
   static const String routeName = '/navigator/web_socket';
@@ -13,11 +14,49 @@ class _WebSocketViewState extends State<WebSocketView> {
   TextEditingController _controller = TextEditingController();
 
   final String title = "채팅 테스트";
-  final WebSocketChannel channel = IOWebSocketChannel.connect('ws://echo.websocket.org');
+  SocketIO socketIO ;
 
   @override
   void initState() {
     super.initState();
+
+    socketIO = SocketIOManager().createSocketIO(
+        "http://pgrt-Chat-13QOMIE6SJQAB.eba-xcpcujs4.ap-northeast-2.elasticbeanstalk.com:80",
+        "/",
+        socketStatusCallback: _socketStatus);
+    socketIO.init();
+    socketIO.subscribe("socket_info", _onSocketInfo);
+    _subscribes();
+    socketIO.connect();
+  }
+
+  _onSocketInfo(dynamic data) {
+    print("Socket info: " + data);
+  }
+
+  _socketStatus(dynamic data) {
+    print("Socket status: " + data);
+  }
+
+  _subscribes() {
+    if (socketIO != null) {
+      socketIO.subscribe("messages", _onReceiveChatMessage);
+      socketIO.subscribe("member_add", _onReceiveChatMessage);
+      socketIO.subscribe("member_delete", _onReceiveChatMessage);
+      socketIO.subscribe("message_history", _onReceiveChatMessage);
+    }
+  }
+
+  void _onReceiveChatMessage(dynamic message) {
+    print("Message from UFO: " + message);
+  }
+
+  void _sendChatMessage() async {
+    String msg = _controller.text;
+    if (socketIO != null) {
+      String jsonData = '{"message":{"type":"Text","content": ${(msg != null && msg.isNotEmpty) ? '"${msg}"' : '"Hello SOCKET IO PLUGIN :))"'},"owner":"589f10b9bbcd694aa570988d","avatar":"img/avatar-default.png"},"sender":{"userId":"589f10b9bbcd694aa570988d","first":"Ha","last":"Test 2","location":{"lat":10.792273999999999,"long":106.6430356,"accuracy":38,"regionId":null,"vendor":"gps","verticalAccuracy":null},"name":"Ha Test 2"},"receivers":["587e1147744c6260e2d3a4af"],"conversationId":"589f116612aa254aa4fef79f","name":null,"isAnonymous":null}';
+      socketIO.sendMessage("chat_direct", jsonData, _onReceiveChatMessage);
+    }
   }
 
   @override
@@ -37,35 +76,34 @@ class _WebSocketViewState extends State<WebSocketView> {
                 decoration: InputDecoration(labelText: 'Send a message'),
               ),
             ),
-            StreamBuilder(
-              stream: channel.stream,
-              builder: (context, snapshot) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 24.0),
-                  child: Text(snapshot.hasData ? '${snapshot.data}' : ''),
-                );
-              },
-            )
+            getMainView(),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _sendMessage,
+        onPressed: _sendChatMessage,
         tooltip: 'Send message',
         child: Icon(Icons.send),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 
-  void _sendMessage() {
-    if (_controller.text.isNotEmpty) {
-      channel.sink.add(_controller.text);
-    }
+  Widget getMainView(){
+    return Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.all(100),
+      child: LinearProgressIndicator(
+        minHeight: 5,
+        backgroundColor: Colors.transparent,
+        valueColor: new AlwaysStoppedAnimation<Color>(Colors.grey),
+      ),
+    );
   }
+
 
   @override
   void dispose() {
-    channel.sink.close();
+    if (socketIO != null) SocketIOManager().destroySocket(socketIO);
     super.dispose();
   }
 }
